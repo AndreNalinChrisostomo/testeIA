f780d926239df739625f605c23b764c40e90240e
AndreNalinChrisostomo <andre.work007@gmail.com>
2023-10-13 23:25:17 -0300

    quase quase

--- teste2.py
+++ teste2.py
@@ -57,33 +57,60 @@ class RedeNeural:
             self.inputs = self.activate_relu(self.inputs)
             print(self.inputs)
             
+
             print(f"########## OUTPUT LAYER: {x+1} ############\n\n")
 
             
-        
+    def calculate_error(self, label): 
+        #calcula o erro medio quadrático
+        n = len(label)
+        #mse = ((label - self.inputs) ** 2).sum() / n
+        mse = np.mean(np.square(label - self.inputs))
+        return mse
+    
+    def derivative(self, label):
+        return 2 * (self.inputs - label)
 
 
     def activate_relu(self, inputs):
         return np.maximum(0, inputs)
+    
+    def update_weights(self, learning_rate, derivative):
+        for x in self.weights:
+            if(derivative.any()):
+                x -= learning_rate
+            else:
+                x += learning_rate
 
 
 if __name__ == "__main__":
-    treino = np.array([[1,0], [0,0], [1,1], [0,1], [1,0], [0,0], [1,1], [0,1], [1,0], [0,0], [0,1], [1,0]])
-    
+    treino = np.array([[6, 9], [4,2], [4,5], [8,1], [1,9], [5,5], [2,6], [2,8], [9,5], [6,4], [3,3], [9,8]])
+    #treino = np.array([5,8])
+    label = np.array([69])
+    label = label.reshape(1,1)
+    learning_rate = 0.01
+
+
     #divide o array em pedaços
-    treino = np.array_split(treino, 6)
+    #treino = np.array_split(treino, 6)
     
     
-    rede = RedeNeural((2, 4, 5, 1))
+    
+    rede = RedeNeural((2, 1))
     
  
     
-    print(treino[0].shape)
-    rede.setInputs(treino[0])
-    rede.setWeights()
+    for x in range(100000):
+        rede.setInputs(treino)
+        rede.setWeights()
+        rede.forward()
 
-    rede.forward()
+        print("############# ERRO MÉDIO QUADRATICO #############")
+        mse = rede.calculate_error(label)
+        print(mse)
 
+        derivada = rede.derivative(label)
+        rede.update_weights(learning_rate, derivada)
 
 
     
